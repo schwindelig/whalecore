cefd1f81e991d6dc1a860b8afb06407397059ec5
schwindelig <david.szoeke@gmail.com>
2017-03-28 07:57:33 +0200

    Add push instructions for build

--- build/build.ps1
+++ build/build.ps1
@@ -1,9 +1,13 @@
+$registryAddress = "cloud.canister.io:5000"
 $registryUsername = "schwindelig"
 $dockerfilesPath = "..\dockerfiles"
 
 # Build & tag base Windows Server image
 $baseWindowsServerFolder = "whalecore-base-windows-server"
 $baseWindowsServerRepoName = "whalecore-base-windows-server"
-docker build --pull -t "$registryUsername/$baseWindowsServerRepoName" (Join-Path $dockerfilesPath $baseWindowsServerFolder)
+$baseWindowsServerTag = "latest"
+$baseWindowsServerFullTag = "$registryAddress/$registryUsername/${baseWindowsServerRepoName}:${baseWindowsServerTag}"
+docker build --pull -t $baseWindowsServerFullTag (Join-Path $dockerfilesPath $baseWindowsServerFolder)
+docker push $baseWindowsServerFullTag
 
 # Build & tag base IIS image
