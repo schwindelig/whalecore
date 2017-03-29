75deabe6fe660bfb7fb29b3380ff3513e03694d0
schwindelig <david.szoeke@gmail.com>
2017-03-29 07:16:57 +0200

    Change to azure registry.

--- build/build.ps1
+++ build/build.ps1
@@ -1,13 +1,14 @@
-$registryAddress = "cloud.canister.io:5000"
-$registryUsername = "schwindelig"
+$registryAddress = "whalecore.azurecr.io"
 $dockerfilesPath = "..\dockerfiles"
 
 # Build & tag base Windows Server image
 $baseWindowsServerFolder = "whalecore-base-windows-server"
 $baseWindowsServerRepoName = "whalecore-base-windows-server"
 $baseWindowsServerTag = "latest"
-$baseWindowsServerFullTag = "$registryAddress/$registryUsername/${baseWindowsServerRepoName}:${baseWindowsServerTag}"
+$baseWindowsServerFullTag = "$registryAddress/${baseWindowsServerRepoName}:${baseWindowsServerTag}"
 docker build --pull -t $baseWindowsServerFullTag (Join-Path $dockerfilesPath $baseWindowsServerFolder)
+Write-Host "Built and tagged $baseWindowsServerFullTag"
 docker push $baseWindowsServerFullTag
+Write-Host "Pushed $baseWindowsServerFullTag"
 
 # Build & tag base IIS image
