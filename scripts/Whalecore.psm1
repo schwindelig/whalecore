37f089372371eeb5abbbe02ba3399765f1a9461f
schwindelig <david.szoeke@gmail.com>
2017-04-01 08:11:25 +0200

    Add PS function for reating local admin.

--- scripts/Whalecore.psm1
+++ scripts/Whalecore.psm1
@@ -1,5 +1,45 @@
 $ErrorActionPreference = "Stop" 
 
+function New-SecureString
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$message
+    )
+
+    return ConvertTo-SecureString $message -AsPlainText -Force
+}
+
+function New-LocalAdmin
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$username,
+        [Parameter(Mandatory=$true)]
+        [SecureString]$password
+    )
+
+    $existingUser = Get-LocalUser $username -ErrorAction Ignore
+    if($existingUser -ne $null){
+        Write-WhalecoreLog "Local user $username already exists."
+    }
+    else{
+        Write-WhalecoreLog "Local user $username was not found. Creating local user."
+        New-LocalUser -Name $username -password $password
+    }
+
+    $adminGroupName = "Administrators"
+    $existingGroupMember = Get-LocalGroupMember -Group $adminGroupName -Member $username -ErrorAction Ignore
+    if($existingGroupMember -ne $null)
+    {
+        Write-WhalecoreLog "$username is already member of group $adminGroupName"
+    }
+    else{
+        Write-WhalecoreLog "Adding $username to group $adminGroupName"
+        Add-LocalGroupMember -Group $adminGroupName -Member $username
+    }
+}
+
 function Write-WhalecoreLog
 {
     param(
